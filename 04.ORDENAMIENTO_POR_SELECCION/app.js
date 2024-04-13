efdcc5e2678230f35078b5250eb891abf09bb723
VaneJasR <vjrr2904@gmail.com>
2024-04-12 19:14:02 -0600

    first commit

---
+++ 04.ORDENAMIENTO_POR_SELECCION/app.js
@@ -0,0 +1,23 @@
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+};
+
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var selectionSort = function(array) {
+
+};
